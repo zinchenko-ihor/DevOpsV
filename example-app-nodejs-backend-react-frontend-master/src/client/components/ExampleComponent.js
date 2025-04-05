8b727ac1193d113732857ee71ffe66c570cb7f2e
Zinchenko Ihor <zinchenkoihor757@gmail.com>
2025-04-05 13:07:19 +0000

    Added fullstack app and Dockerfile

---
+++ example-app-nodejs-backend-react-frontend-master/src/client/components/ExampleComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ExampleComponent() {
+    return (
+        <div>
+            <h1>Welcome to My Project</h1>
+            <h2>Created by Ihor Zinchenko</h2>
+        </div>
+    );
+}
